869c08b18a2459127df7935edf4cf5d9683854f1
Mike Solomon <mike120982@gmail.com>
2022-08-20 18:09:00 +0300

    Adds comment

--- src/Paraglider/Operator/Lift.purs
+++ src/Paraglider/Operator/Lift.purs
@@ -4,6 +4,9 @@ import Prelude
 
 import FRP.Event (AnEvent, makeEvent)
 
+-- | Lifts a monad into event, resulting in a single emission.
+-- | Different that `pure` in that it also triggers the monadic side effect
+-- | in addition to emitting the value.
 lift :: ∀ a m. Monad m => m a -> AnEvent m a
 lift monad = makeEvent \k -> do
   emission <- monad
