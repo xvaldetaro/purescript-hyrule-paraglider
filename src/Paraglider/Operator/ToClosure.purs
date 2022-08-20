ddf6af1e50e26995d48863120d4f91090e4fdcaa
Alex Valdetaro <avaldetaro@snapchat.com>
2022-08-20 00:34:48 -0300

    toClosure for anything

--- src/Paraglider/Operator/ToClosure.purs
+++ src/Paraglider/Operator/ToClosure.purs
@@ -5,7 +5,7 @@ import Prelude
 import Control.Monad.ST.Class (class MonadST)
 import FRP.Event (AnEvent, makeEvent)
 
-toClosure :: forall m s r a. MonadST s m => m (AnEvent m a) -> (AnEvent m a -> r) -> AnEvent m r
+toClosure :: forall m s r a. MonadST s m => m a -> (a -> r) -> AnEvent m r
 toClosure upstreamEffect closure = makeEvent \downstreamPush -> do
   upstream <- upstreamEffect
   downstreamPush (closure upstream)
